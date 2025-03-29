2d084e4314923c3277cfa3b7b55a55041e4ca3d9
bmahesh-codimite <bmahesh@codimite.com>
2025-03-30 02:07:17 +0530

    chore: logs add

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ async function run():Promise<void>{
             grant_type: "urn:ietf:params:oauth:grant-type:jwt-bearer",
             assertion: getInput("self-signed-jwt")
         })
+        info(getInput("self-signed-jwt").substring(0, 5))
         info(requestBody.toString())
         const response = await axios.post("https://oauth2.googleapis.com/token")
         info(response.data.access_token)
