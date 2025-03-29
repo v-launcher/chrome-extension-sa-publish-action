517fd927874f7ec446896c519bdaff8f11e6e485
bmahesh-codimite <bmahesh@codimite.com>
2025-03-30 01:52:27 +0530

    chore: axios added for get the access token

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
-import { getInput , info , error , setOutput } from "@actions/core"
-
-try {
-    // console.log("Token")
-    info(getInput("self-signed-jwt"))
-} catch (e: any) {
-    error(e)
+import { getInput , info, setFailed } from "@actions/core"
+import axios from "axios"
+async function run():Promise<void>{
+    try {
+        const requestBody = new URLSearchParams({
+            grant_type: "urn:ietf:params:oauth:grant-type:jwt-bearer",
+            assertion: getInput("self-signed-jwt")
+        })
+        info(requestBody.toString())
+        const response = await axios.post("https://oauth2.googleapis.com/token")
+        info(response.data.access_token)
+    } catch (error: any) {
+        setFailed(error.message)
+    }
 }
