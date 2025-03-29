faad937d7c56c9d97bfaa89181efff928755bc65
bmahesh-codimite <bmahesh@codimite.com>
2025-03-30 01:57:43 +0530

    fix: builds created

--- out/index.js
+++ out/index.js
@@ -1,10 +1,33 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@actions/core");
-try {
-    // console.log("Token")
-    (0, core_1.info)((0, core_1.getInput)("self-signed-jwt"));
+const axios_1 = __importDefault(require("axios"));
+function run() {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            const requestBody = new URLSearchParams({
+                grant_type: "urn:ietf:params:oauth:grant-type:jwt-bearer",
+                assertion: (0, core_1.getInput)("self-signed-jwt")
+            });
+            (0, core_1.info)(requestBody.toString());
+            const response = yield axios_1.default.post("https://oauth2.googleapis.com/token");
+            (0, core_1.info)(response.data.access_token);
+        }
+        catch (error) {
+            (0, core_1.setFailed)(error.message);
+        }
+    });
 }
-catch (e) {
-    (0, core_1.error)(e);
-}
+run();
