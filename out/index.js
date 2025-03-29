30d9e75eb499786451ed474fc34782901a455e5b
bmahesh-codimite <bmahesh@codimite.com>
2025-03-30 02:59:37 +0530

    feat: chrome webstore token generation added

--- out/index.js
+++ out/index.js
@@ -41,30 +41,53 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@actions/core");
 const axios_1 = __importStar(require("axios"));
+const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
-        var _a, _b;
+        var _a;
         try {
+            const serviceAccount = JSON.parse((0, core_1.getInput)("service-account-json"));
+            (0, core_1.info)("Email" + serviceAccount.client_email);
+            const now = Math.floor(Date.now() / 1000);
+            const payload = {
+                iss: serviceAccount.client_email,
+                scope: "https://www.googleapis.com/auth/chromewebstore", // API scope
+                aud: "https://oauth2.googleapis.com/token", // Google token URL
+                exp: now + 3600,
+                iat: now,
+                sub: (0, core_1.getInput)("impersonated-user-email")
+            };
+            const idToken = jsonwebtoken_1.default.sign(payload, serviceAccount.private_key, { algorithm: "RS256" });
             const requestBody = new URLSearchParams({
                 grant_type: "urn:ietf:params:oauth:grant-type:jwt-bearer",
-                assertion: (0, core_1.getInput)("self-signed-jwt")
+                assertion: idToken
             });
-            (0, core_1.info)((0, core_1.getInput)("self-signed-jwt").substring(0, (0, core_1.getInput)("self-signed-jwt").length));
-            (0, core_1.info)(requestBody.toString());
             const response = yield axios_1.default.post("https://oauth2.googleapis.com/token", requestBody.toString(), {
                 headers: { "Content-Type": "application/x-www-form-urlencoded" }
             });
-            (0, core_1.info)(response.data.access_token);
+            const extensionId = (0, core_1.getInput)("chrome-extension-id");
+            const extensionResponse = yield axios_1.default.get(`https://www.googleapis.com/chromewebstore/v1.1/items/${extensionId}?projection=DRAFT`, {
+                headers: {
+                    "Authorization": `Bearer ${response.data.access_token}`
+                }
+            });
+            (0, core_1.info)(extensionResponse.data);
         }
         catch (error) {
             if ((0, axios_1.isAxiosError)(error)) {
-                (0, core_1.error)(JSON.stringify((_a = error.response) === null || _a === void 0 ? void 0 : _a.data));
-                (0, core_1.setFailed)((_b = error.response) === null || _b === void 0 ? void 0 : _b.data);
+                (0, core_1.setFailed)((_a = error.response) === null || _a === void 0 ? void 0 : _a.data);
+                return;
             }
+            (0, core_1.setFailed)(error.message);
         }
     });
 }
-run();
+if (require.main === module) {
+    run();
+}
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,44 @@
 import { getInput , info, setFailed ,error as errorLog } from "@actions/core"
 import axios, { isAxiosError } from "axios"
+import jwt from "jsonwebtoken"
+
 async function run():Promise<void>{
     try {
+        const serviceAccount = JSON.parse(getInput("service-account-json"))
+        info("Email"+serviceAccount.client_email)
+        const now = Math.floor(Date.now() / 1000);
+        const payload = {
+            iss: serviceAccount.client_email,
+            scope: "https://www.googleapis.com/auth/chromewebstore", // API scope
+            aud: "https://oauth2.googleapis.com/token", // Google token URL
+            exp: now + 3600,
+            iat: now,
+            sub: getInput("impersonated-user-email")
+        }
+        const idToken = jwt.sign(payload, serviceAccount.private_key, {algorithm:"RS256"})
         const requestBody = new URLSearchParams({
             grant_type: "urn:ietf:params:oauth:grant-type:jwt-bearer",
-            assertion: getInput("self-signed-jwt")
-        })
-        info(getInput("self-signed-jwt").substring(0, getInput("self-signed-jwt").length))
-        info(requestBody.toString())
+            assertion: idToken
+        }) 
         const response = await axios.post("https://oauth2.googleapis.com/token",requestBody.toString(),{
             headers: { "Content-Type": "application/x-www-form-urlencoded"}
         })
-        info(response.data.access_token)
+        const extensionId = getInput("chrome-extension-id")
+        const extensionResponse = await axios.get(`https://www.googleapis.com/chromewebstore/v1.1/items/${extensionId}?projection=DRAFT`,{
+            headers:{
+                "Authorization" :`Bearer ${response.data.access_token}`
+            }
+        })
+        info(extensionResponse.data)
     } catch (error: any) {
         if (isAxiosError(error)){
-            errorLog(JSON.stringify(error.response?.data))
             setFailed(error.response?.data)
+            return
         }
+        setFailed(error.message)
     }
 }
 
-run()
+if(require.main === module){
+    run()
+}
